54140fc6360a4ba2a499d2d68660ac084c0b13a0
jaehoon <sjh7877@naver.com>
2024-04-03 23:17:12 +0900

    Feat: CGColor static 구현

---
+++ Projects/Modules/DSKit/Sources/Color/CGColor+Extension.swift
@@ -0,0 +1,30 @@
+//
+//  CGColor+Extension.swift
+//  DSKit
+//
+//  Created by Seo Jae Hoon on 4/3/24.
+//  Copyright © 2024 Jacsim. All rights reserved.
+//
+
+import UIKit
+
+public extension CGColor {
+    
+    static let primaryNormal = UIColor.primaryNormal.cgColor
+    static let primaryStrong = UIColor.primaryStrong.cgColor
+    static let primaryHeavy = UIColor.primaryHeavy.cgColor
+    
+    static let labelNormal = UIColor.primaryHeavy.cgColor
+    static let labelStrong = UIColor.labelStrong.cgColor
+    static let labelNeutral = UIColor.labelNeutral.cgColor
+    static let labelAlternative = UIColor.labelAlternative.cgColor
+    static let labelAssistive = UIColor.labelAssistive.cgColor
+    static let labelDisable = UIColor.labelDisable.cgColor
+    
+    static let backgroundNormal = UIColor.backgroundNormal.cgColor
+    
+    static let positive = UIColor.positive.cgColor
+    static let cautionary = UIColor.cautionary.cgColor
+    static let destructive = UIColor.destructive.cgColor
+}
+
