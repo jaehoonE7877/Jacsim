3c65acde0270ec3e2ad8b2c70216d10b98c64d1b
서재훈 <sjh7877@hsociety.co.kr>
2025-11-13 01:00:09 +0900

    Build: iOS 18.0 최소타깃 및 Xcode 26.x 고정

--- Tuist.swift
+++ Tuist.swift
@@ -9,6 +9,7 @@ import ProjectDescription
 
 let config = Config.init(
     project: TuistProject.tuist(
+        // Xcode 26.x 고정(최소 26, 27+ 포함 필요 시 from("26.0")로 전환)
         compatibleXcodeVersions: CompatibleXcodeVersions.upToNextMajor("26.0"),
         swiftVersion: Version(string: "6.0"),
         plugins: [
