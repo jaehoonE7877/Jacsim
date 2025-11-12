137c2f3e66584b8b80ea4fb3fc951a59aec7a1f6
서재훈 <sjh7877@hsociety.co.kr>
2025-11-13 00:40:11 +0900

    Fix: Firebase Crashlytics 스크립트 수정

--- Tuist/ProjectDescriptionHelpers/Project+Templates.swift
+++ Tuist/ProjectDescriptionHelpers/Project+Templates.swift
@@ -277,13 +277,13 @@ extension Project {
 public extension TargetScript {
     static let FirebaseCrashlyticsString = TargetScript.post(
         script: """
-    ROOT_DIR=\(ProcessInfo.processInfo.environment["TUIST_ROOT_DIR"] ?? "")
-    "${ROOT_DIR}/.build/checkouts/firebase-ios-sdk/Crashlytics/run"
+        PROJECT_ROOT="$SRCROOT/../.."
+        "$PROJECT_ROOT/.build/checkouts/firebase-ios-sdk/Crashlytics/run"
     """,
         name: "Firebase Crashlytics",
         inputPaths: [
             "${DWARF_DSYM_FOLDER_PATH}/${DWARF_DSYM_FILE_NAME}/Contents/Resources/DWARF/${TARGET_NAME}",
                 "$(SRCROOT)/$(BUILT_PRODUCTS_DIR)/$(INFOPLIST_PATH)"
         ],
-        basedOnDependencyAnalysis: true)
+        basedOnDependencyAnalysis: false)
 }
