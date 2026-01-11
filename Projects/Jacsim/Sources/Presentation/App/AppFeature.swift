bdaaa91d520c487eb39bc7428e32ae5846556a0b
서재훈 <sjh7877@hsociety.co.kr>
2026-01-11 21:33:55 +0900

    앱 루트 기능과 화면 추가

---
+++ Projects/Jacsim/Sources/Presentation/App/AppFeature.swift
@@ -0,0 +1,45 @@
+import Foundation
+import ComposableArchitecture
+
+@Reducer
+public struct AppFeature {
+    @ObservableState
+    public enum State: Equatable {
+        case onboarding(WalkThroughFeature.State)
+        case main(HomeFeature.State)
+        
+        public init() {
+            let isOnboarded = UserDefaults.standard.bool(forKey: "onboarding")
+            if isOnboarded {
+                self = .main(HomeFeature.State())
+            } else {
+                self = .onboarding(WalkThroughFeature.State(fromSetting: false))
+            }
+        }
+    }
+
+    public enum Action {
+        case onboarding(WalkThroughFeature.Action)
+        case main(HomeFeature.Action)
+    }
+
+    public var body: some ReducerOf<Self> {
+        Reduce { state, action in
+            switch action {
+            case let .onboarding(.delegate(.completeOnboarding)):
+                UserDefaults.standard.set(true, forKey: "onboarding")
+                state = .main(HomeFeature.State())
+                return .none
+                
+            case .onboarding, .main:
+                return .none
+            }
+        }
+        .ifCaseLet(\.onboarding, action: \.onboarding) {
+            WalkThroughFeature()
+        }
+        .ifCaseLet(\.main, action: \.main) {
+            HomeFeature()
+        }
+    }
+}
---
+++ Projects/Jacsim/Sources/Presentation/App/AppView.swift
@@ -0,0 +1,27 @@
+import SwiftUI
+import ComposableArchitecture
+
+public struct AppView: View {
+    let store: StoreOf<AppFeature>
+
+    public init(store: StoreOf<AppFeature>) {
+        self.store = store
+    }
+
+    public var body: some View {
+        Group {
+            switch store.state {
+            case .onboarding:
+                if let onboardingStore = store.scope(state: \.onboarding, action: \.onboarding) {
+                    WalkThroughView(store: onboardingStore)
+                }
+            case .main:
+                if let mainStore = store.scope(state: \.main, action: \.main) {
+                    NavigationStack {
+                        HomeView(store: mainStore)
+                    }
+                }
+            }
+        }
+    }
+}
