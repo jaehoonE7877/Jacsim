22f3139fc16aa81d70573a5affd806461b4eabd5
서재훈 <sjh7877@hsociety.co.kr>
2026-01-11 21:30:34 +0900

    새 작심 기능과 뷰 갱신

---
+++ Projects/Jacsim/Sources/Presentation/NewTask/NewTaskFeature.swift
@@ -0,0 +1,107 @@
+import Foundation
+import ComposableArchitecture
+import SwiftUI
+
+@Reducer
+public struct NewTaskFeature {
+    @ObservableState
+    public struct State: Equatable {
+        public var title: String = ""
+        public var startDate: Date = Date()
+        public var endDate: Date = Date().addingTimeInterval(86400 * 7)
+        public var successCount: Int = 1
+        public var image: UIImage?
+        public var alarmDate: Date = Date()
+        public var isAlarmEnabled: Bool = false
+        
+        public var path = StackState<Path.State>()
+        @Presents public var alert: AlertState<Action.Alert>?
+        
+        public init() {}
+    }
+
+    public enum Action: BindableAction {
+        case binding(BindingAction<State>)
+        case path(StackAction<Path.State, Path.Action>)
+        case saveButtonTapped
+        case saveCompleted
+        case nextButtonTapped
+        case confirmAlarmButtonTapped
+        case cancelButtonTapped
+        case alert(PresentationAction<Alert>)
+        case delegate(Delegate)
+        
+        public enum Alert: Equatable {
+            case dismiss
+        }
+        
+        public enum Delegate {
+            case taskCreated
+        }
+    }
+
+    public struct Path: Reducer {
+        @ObservableState
+        @CasePathable
+        @dynamicMemberLookup
+        public enum State: Equatable, Hashable {
+            case alarmInput
+            case summary
+        }
+        @CasePathable
+        @dynamicMemberLookup
+        public enum Action: Equatable {
+            case alarmInput
+            case summary
+        }
+        public var body: some ReducerOf<Self> {
+            EmptyReducer()
+        }
+    }
+
+    @Dependency(\.jacsimClient) var jacsimClient
+
+    public var body: some ReducerOf<Self> {
+        BindingReducer()
+        Reduce { state, action in
+            switch action {
+            case .nextButtonTapped:
+                state.path.append(.alarmInput)
+                return .none
+            case .confirmAlarmButtonTapped:
+                state.path.append(.summary)
+                return .none
+            case .saveButtonTapped:
+                return .run { [state] send in
+                    let task = UserJacsim(
+                        title: state.title,
+                        startDate: state.startDate,
+                        endDate: state.endDate,
+                        success: state.successCount,
+                        alarm: state.isAlarmEnabled ? state.alarmDate : nil
+                    )
+                    await jacsimClient.addJacsim(task)
+                    await send(.saveCompleted)
+                    await send(.delegate(.taskCreated))
+                }
+            case .saveCompleted:
+                state.alert = AlertState {
+                    TextState("저장 완료")
+                } actions: {
+                    ButtonState(role: .cancel, action: .send(.dismiss)) {
+                        TextState("확인")
+                    }
+                } message: {
+                    TextState("작심이 저장되었습니다")
+                }
+                return .none
+            case .binding, .path, .cancelButtonTapped, .delegate, .alert:
+                return .none
+            }
+        }
+        .ifLet(\.alert, action: \.alert)
+        .forEach(\.path, action: \.path) {
+            Path()
+        }
+    }
+}
